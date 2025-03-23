c413426d1e509407d46fb7ee346acbf7432ade25
OlenaKut <105447315+OlenaKut@users.noreply.github.com>
2025-03-23 19:38:14 +0100

    Improved the code

--- ConsoleApp1/IfElseLesson.cs
+++ ConsoleApp1/IfElseLesson.cs
@@ -12,26 +12,23 @@ class IfElseLesson
         Console.Write("Choose a door: 1, 2, or 3: ");
         string userValue = Console.ReadLine() ?? string.Empty;
 
+        string message = "";
+
         if (userValue == "1")
-        {
-            string message = "You won a new car!";
-            Console.WriteLine(message);
-        }
+            message = "You won a new car!";
         else if (userValue == "2")
-        {
-            string message = "You won a new boat!";
-            Console.WriteLine(message);
-        }
+            message = "You won a new boat!";
         else if (userValue == "3")
-        {
-            string message = "You won a new cat!";
-            Console.WriteLine(message);
-        }
+            message = "You won a new cat!";
         else
-        {
-            string message = "Sorry, we didn't understand. You lose!";
-            Console.WriteLine(message);
-        }
+            message = "Sorry, we didn't understand.";
+        //message = message + "You lose!";
+        message += "You lose!";
+
+
+        Console.WriteLine(message);
+        //Console.ReadLine();
 
     }
+
 }
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a58d499781c58a19724f0ecd550a87afd9106d4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f522a88971f5144ca93b2e383fcc99fd10b98c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
