6e9debf7feecd79608bf46290d6a135220d46da8
OlenaKut <105447315+OlenaKut@users.noreply.github.com>
2025-04-02 10:53:48 +0200

    Bool lesson

---
+++ ConsoleApp1/BoolLesson.cs
@@ -0,0 +1,32 @@
+namespace BoolNamespace
+{
+    class BoolLesson
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+            bool a = age == 23;
+
+            Console.WriteLine("Age is greater than 23: " + a);
+
+
+            bool b = age >= 23;
+            Console.WriteLine("Age is less than 23: " + b);
+
+            bool c = (age >= 18) && (age <= 65);
+            Console.WriteLine("Age is between 18 and 65: " + c);
+
+            int age1 = 16;
+            int age2 = 64;
+
+            bool d = (age1 >= 18) && (age2 <= 65);
+            Console.WriteLine("Age1 is greater than 18 AND age2 is less than 65: " + d);
+            
+            bool e = (age1 >= 18) || (age2 <= 65);
+            Console.WriteLine("Age1 is greater than 18 OR age2 is less than 65: " + e);
+
+
+
+        }
+    }
+}
